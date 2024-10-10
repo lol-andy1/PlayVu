35750ae1f0906da2a93d22e2f04af2971d103050
3garos <emgd@tamu.edu>
2024-10-10 11:53:39 -0500

    Connected to supabase database and added users table

---
+++ src/main/java/com/playvu/backend/entities/Users.java
@@ -0,0 +1,34 @@
+package com.playvu.backend.entities;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column()
+    private String role;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+}
