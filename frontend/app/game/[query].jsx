2a5baa948b8366b9b875d035d75561ab2cbb2881
lol-andy1 <a.c@tamu.edu>
2024-11-04 17:53:29 -0600

    fetch game data from backend

--- frontend/app/game/[query].jsx
+++ frontend/app/game/[query].jsx
@@ -1,202 +1,98 @@
 import { StatusBar } from "expo-status-bar";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import {View, Text, Image, ScrollView, Alert} from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios"
 import CustomButton from "../../components/CustomButton";
 import Feather from "@expo/vector-icons/Feather";
 import FootballField from "react-native-football-lineup";
 import AppHeader from "../../components/AppHeader";
 
-var home = {
-  name: "POR",
-  module: "4-4-2",
-  team: [
-    [
-      {
-        number: 1,
-        name: "Patricio",
-      },
-    ],
-    [
-      {
-        number: 21,
-        name: "Soares",
-      },
-      {
-        number: 3,
-        name: "Pepe",
-      },
-      {
-        number: 6,
-        name: "Fonte",
-      },
-      {
-        number: 5,
-        name: "Guerriero",
-      },
-    ],
-    [
-      {
-        number: 14,
-        name: "Calvalho",
-      },
-      {
-        number: 8,
-        name: "Mountinho",
-      },
-      {
-        number: 11,
-        name: "Silva",
-      },
-      {
-        number: 17,
-        name: "Guedes",
-      },
-    ],
-    [
-      {
-        number: 16,
-        name: "Fernandes",
-      },
-      {
-        number: 7,
-        name: "Cristiano Ronaldo",
-      },
-    ],
-  ],
-  home_team_events: [
-    {
-      id: 203,
-      type_of_event: "red-card",
-      player: "Silva",
-      time: "3'",
-    },
-    {
-      id: 210,
-      type_of_event: "yellow-card",
-      player: "Fernandes",
-      time: "64'",
-    },
-    {
-      id: 210,
-      type_of_event: "yellow-card",
-      player: "Fonte",
-      time: "64'",
-    },
-    {
-      id: 206,
-      type_of_event: "substitution-in",
-      player: "Fonte",
-      time: "31'",
-    },
-  ],
-};
-
-var away = {
-  name: "SPA",
-  module: "4-2-3-1",
-  team: [
-    [
-      {
-        number: 1,
-        name: "De Gea",
-      },
-    ],
-    [
-      {
-        number: 18,
-        name: "Alba",
-      },
-      {
-        number: 15,
-        name: "Ramos",
-      },
-      {
-        number: 3,
-        name: "Pique",
-      },
-      {
-        number: 4,
-        name: "Nacho",
-      },
-    ],
-    [
-      {
-        number: 8,
-        name: "Koke",
-      },
-      {
-        number: 5,
-        name: "Busquets",
-      },
-    ],
-    [
-      {
-        number: 6,
-        name: "Iniesta",
-      },
-      {
-        number: 22,
-        name: "Isco",
-      },
-      {
-        number: 21,
-        name: "Silva",
-      },
-    ],
-    [
-      {
-        number: 19,
-        name: "Costa",
-      },
-    ],
-  ],
-  away_team_events: [
-    {
-      id: 210,
-      type_of_event: "yellow-card",
-      player: "De Gea",
-      time: "12'",
-    },
-    {
-      id: 206,
-      type_of_event: "substitution-in",
-      player: "Iniesta",
-      time: "31'",
-    },
-    {
-      id: 206,
-      type_of_event: "substitution-in",
-      player: "Costa",
-      time: "32'",
-    },
-    {
-      id: 206,
-      type_of_event: "red-card",
-      player: "Silva",
-      time: "31'",
-    },
-  ],
-};
-
 const Search = () => {
-  const { query } = useLocalSearchParams();
-  const router = useRouter();
-  //   const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
-  //   useEffect(() => {
-  //     refetch();
-  //   }, [query]);
-  const gameData = {
-    "startTime": new Date(2024, 9, 25, 10, 30, 0),
-    "maxPlayers": 22
+  const homeFormation = "4-3-3"
+  const [homePlayers, setHomePlayers] = useState([])
+  const awayFormation = "4-3-3"
+  const [awayPlayers, setAwayPlayers] = useState([])
+
+  const home = {  
+    name: "SPA",
+    module: homeFormation,
+    team: homePlayers,
+    home_team_events: []
   }
+
+  const away = {  
+    name: "FRA",
+    module: awayFormation,
+    team: awayPlayers,
+    away_team_events: []
+  }
+
+  const [gameName, setGameName] = useState("")
+  const [startDate, setStartDate] = useState(new Date(""))
+  const [price, setPrice] = useState(0)
+
+  const setPositions = (players, formation, flag) => {
+    formation = "1-" + formation
+    formation = formation.split("-").map(Number)
+    team = []
+    let playerIndex = 0
+    
+    formation.forEach((count, index) => {
+      team[index] = []
+      for (let i = 0; i < count; i++){
+        if (playerIndex < players.length){
+          team[index].push({number: playerIndex, name: players[playerIndex]})
+          playerIndex++
+        }
+      }
+    });
+
+    if (flag){
+      setAwayPlayers(team)
+    }
+    else{
+      setHomePlayers(team)
+    }
+  }
+
+  const { query } = useLocalSearchParams();
+
+  useEffect(() => {
+    const getGameData = async () => {
+      try {
+        const res = await axios.get('http://localhost:8080/api/get-game-data',{
+          params: {
+            gameId: query
+          }
+        })
+        console.log(res.data)
+
+        setPositions(res.data.team_1, homeFormation, false)
+        setPositions(res.data.team_2, awayFormation, true)
+
+        setGameName(res.data.name)
+        setStartDate(new Date(res.data.start_date))
+        setPrice(res.data.price)
+
+      } catch(err) {
+        console.error(err);
+      }
+    }
+
+    getGameData()
+  }, [query]) 
+
+  const router = useRouter();
+
   const currPlayerCount = home.team.reduce((len, arr) => len + arr.length, 0) 
     + away.team.reduce((len, arr) => len + arr.length, 0)
 
   return (
     <View className="bg-white h-full">
       <AppHeader 
-        title={`Game ${query}`}
+        title={gameName}
         leftComp={
           <CustomButton
             title={<Feather name="arrow-left" size={25} />}
@@ -216,14 +112,14 @@ const Search = () => {
         />
 
         <View className="px-4 flex-row justify-between">
-          <Text className="text-center text-sm text-black">{gameData.startTime.toLocaleString()}</Text>
-          <Text className="text-center text-sm text-black">Price: ${query}</Text>
+          <Text className="text-center text-sm text-black">{startDate.toLocaleString()}</Text>
+          <Text className="text-center text-sm text-black">Price: ${price}</Text>
         </View>
         
         <View className="flex px-4 flex-row justify-between items-center">
         <Text className="text-lg font-semibold">Roster</Text>
           <Text className="text-sm text-black">
-            Players: {currPlayerCount}/{gameData.maxPlayers}
+            Players: {currPlayerCount}/{22}
           </Text>
         </View>
 
