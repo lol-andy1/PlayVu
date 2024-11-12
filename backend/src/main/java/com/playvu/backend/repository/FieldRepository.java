67988fa4286b63d9f9084abc8eadc7ff565671ac
3garos <emgd@tamu.edu>
2024-11-11 19:47:42 -0600

    added more description to field route

--- backend/src/main/java/com/playvu/backend/repository/FieldRepository.java
+++ backend/src/main/java/com/playvu/backend/repository/FieldRepository.java
@@ -27,7 +27,7 @@ public interface FieldRepository extends JpaRepository<Field, Integer> {
            nativeQuery = true)
     List<Integer> findNearestFields(@Param("latitude") Float latitude, @Param("longitude") Float longitude, @Param("distance") Float distance);
 
-    @Query(value = "SELECT f.name AS \"fieldName\", f.field_id AS \"fieldId\" FROM field f WHERE f.owner_id = :ownerId", nativeQuery = true)
+    @Query(value = "SELECT f.name AS \"fieldName\", f.address AS \"address\", f.zip_code AS \"zipCode\", f.city AS \"city\", f.field_id AS \"fieldId\" FROM field f WHERE f.owner_id = :ownerId", nativeQuery = true)
     List < Map<String, Object> > findByOwnerId(@Param("ownerId") Integer ownerId);
 
 }
