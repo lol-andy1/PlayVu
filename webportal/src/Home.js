51fa231c2171e7eda221c6e8e6442b03f2740d53
rushil.jayant@gmail.com <rushil.jayant@gmail.com>
2024-11-15 22:06:26 -0600

    Nicer Home portal with user/captain features

--- webportal/src/Home.js
+++ webportal/src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <header className="w-full max-w-5xl text-center mb-12">
         <div className="flex flex-col md:flex-row items-center justify-center bg-white rounded-3xl shadow-xl py-10">
           <div className="max-w-sm">
-            <h1 className="text-4xl font-extrabold mb-6">Playtime made eazy</h1>
+            <h1 className="text-4xl font-extrabold mb-6">Playtime made easy</h1>
             <p className="text-xl text-gray-700">
               Organizing and participating in casual sports is now easier and
               more enjoyable than ever! Join the community, find games, and make
@@ -24,38 +24,53 @@ const Home = () => {
         </div>
       </header>
 
-      <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
-        <FeatureCard
-          name={"Field Management"}
-          description={
-            "Update availability, assign subfields, and keep schedules organized with ease."
-          }
-        />
-
-        <FeatureCard
-          name={"Game Coordination"}
-          description={
-            "Effortlessly schedule games and notify team captains to keep \
+      <div className="bg-white rounded-3xl shadow-xl py-10 flex flex-col items-center my-10">
+        <h1 className="text-4xl font-extrabold mb-6 text-center ">Users</h1>
+        <p className="max-w-3xl text-lg text-gray-600 mb-10 text-center mx-10">
+          Provide players with a straightforward and flexible experience to
+          find, join, and participate in local sports games.
+        </p>
+        <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
+          <FeatureCard
+            name={"Game Coordination"}
+            description={
+              "Effortlessly schedule games and notify team captains to keep \
         everyone in sync."
-          }
-        />
+            }
+          />
 
-        <FeatureCard
-          name={"Availability Optimization"}
-          description={
-            "Enable captains to reserve fields based on real-time availability, \
-        simplifying booking for all teams."
-          }
-        />
-
-        <FeatureCard
-          name={"Instant Notifications"}
-          description={
-            "Stay informed with real-time updates for scheduling changes, field \
+          <FeatureCard
+            name={"Instant Notifications"}
+            description={
+              "Stay informed with real-time updates for scheduling changes, field \
         availability, and more."
-          }
-        />
-      </section>
+            }
+          />
+        </section>
+      </div>
+
+      <div className="bg-white rounded-3xl shadow-xl py-10 flex flex-col items-center">
+        <h1 className="text-4xl font-extrabold mb-6 text-center ">Captains</h1>
+        <p className="max-w-3xl text-lg text-gray-600 mb-10 text-center mx-10">
+          Enable captains to organize and manage games efficiently.
+        </p>
+        <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
+          <FeatureCard
+            name={"Field Management"}
+            description={
+              "Update availability, assign subfields, and keep schedules organized with ease."
+            }
+          />
+
+          <FeatureCard
+            name={"Availability Optimization"}
+            description={
+              "Enable captains to reserve fields based on real-time availability, \
+        simplifying booking for all teams."
+            }
+          />
+        </section>
+      </div>
     </div>
   );
 };
