ed7c61172d52d1f3af009f7de8098df1256b7286
rushil.jayant@gmail.com <rushil.jayant@gmail.com>
2024-11-18 20:42:41 -0600

    complete home page

--- webportal/src/Home.js
+++ webportal/src/Home.js
@@ -47,12 +47,26 @@ const Home = () => {
           />
 
           <FeatureCard
+            name={"Secure Payments"}
+            description={
+              "Pay fees through in-app Stripe integration and secure in-app transactions."
+            }
+          />
+
+          <FeatureCard
+            name={"Personalized Profile"}
+            description={
+              "Create a profile that represents your information to share with captains and field owners."
+            }
+          />
+
+          {/* <FeatureCard
             name={"Instant Notifications"}
             description={
               "Stay informed with real-time updates for scheduling changes, field \
         availability, and more."
             }
-          />
+          /> */}
         </section>
       </div>
 
@@ -63,9 +77,39 @@ const Home = () => {
         </p>
         <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
           <FeatureCard
-            name={"Field Management"}
+            name={"Create Games Quickly"}
             description={
-              "Update availability, assign subfields, and keep schedules organized with ease."
+              "Set up new games and venues, as well as coordinate with players and field owners"
+            }
+          />
+
+          <FeatureCard
+            name={"Reserve Fields and Invite Players"}
+            description={
+              "Browse available fields for games, reserve them, and let people join games"
+            }
+          />
+
+          <FeatureCard
+            name={"Monitor Players"}
+            description={
+              "Keep track of who joins games, manage teams, and share updates"
+            }
+          />
+        </section>
+      </div>
+
+      <div className="bg-white rounded-3xl shadow-xl py-10 flex flex-col items-center mt-10">
+        <h1 className="text-4xl font-extrabold mb-6 text-center ">Owners</h1>
+        <p className="max-w-3xl text-lg text-gray-600 mb-10 text-center mx-10">
+          Maximize revenue by improving utilization and simplifying management
+          for fields
+        </p>
+        <section className="w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
+          <FeatureCard
+            name={"List your fields"}
+            description={
+              "Showcase your field and facilities to an audience looking to book venues for players"
             }
           />
 
@@ -74,6 +118,20 @@ const Home = () => {
             description={
               "Enable captains to reserve fields based on real-time availability, \
         simplifying booking for all teams."
+            }
+          />
+
+          <FeatureCard
+            name={"Update field availability information"}
+            description={
+              "Manage fields and sub field avaiablity for bookings in real-time"
+            }
+          />
+
+          <FeatureCard
+            name={"View Schedules At a Glance"}
+            description={
+              "Browse available fields for games, reserve them, and let people join games"
             }
           />
         </section>
