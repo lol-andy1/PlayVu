0b72b6efe8f26ce910c8d3410e8a4d4d123b4232
3garos <emgd@tamu.edu>
2024-11-24 14:18:05 -0600

    added picture for get field

--- backend/src/main/java/com/playvu/backend/repository/GameRepository.java
+++ backend/src/main/java/com/playvu/backend/repository/GameRepository.java
@@ -17,7 +17,7 @@ import com.playvu.backend.entity.Game;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Integer> {
 
-    @Query(value = "SELECT g.game_id, g.max_players, g.sub_field_id, g.organizer_id, g.name, g.start_date AT TIME ZONE 'UTC', g.end_date AT TIME ZONE 'UTC', f.address AS \"location\", " +
+    @Query(value = "SELECT g.game_id, g.max_players, g.sub_field_id, g.organizer_id, g.name, g.start_date AT TIME ZONE 'UTC', g.end_date AT TIME ZONE 'UTC', f.picture, f.address AS \"location\", " +
                     "(SELECT COUNT(gp.participant_id) FROM game_participant gp WHERE gp.game_id = g.game_id) AS \"playerCount\" " +
                     "FROM game g " +
                     "JOIN sub_field sf ON g.sub_field_id = sf.sub_field_id " +
@@ -42,7 +42,7 @@ public interface GameRepository extends JpaRepository<Game, Integer> {
        nativeQuery = true)
     List< Map<String, Object> > findByOrganizerId(@Param("organizerId") Integer organizerId);
 
-    @Query(value = "SELECT g.game_id, g.max_players, g.name, g.start_date AT TIME ZONE 'UTC', g.end_date AT TIME ZONE 'UTC', g.price, mf.address AS \"location\", " +
+    @Query(value = "SELECT g.game_id, g.max_players, g.name, g.start_date AT TIME ZONE 'UTC', g.end_date AT TIME ZONE 'UTC', g.price, mf.picture, mf.address AS \"location\", " +
                "(SELECT COUNT(gp.participant_id) FROM game_participant gp WHERE gp.game_id = g.game_id) AS \"playerCount\", " + 
                "sf.name AS sub_field_name, " +
                "mf.name AS master_field_name " +
